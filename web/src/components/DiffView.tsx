14a6f4f00a9fb29ca638ef97a6e0742de85010be
Harry Lawton <harrisonzimbo777@gmail.com>
2026-08-28 13:28:33 +0100

    diffs

--- web/src/components/DiffView.tsx
+++ web/src/components/DiffView.tsx
@@ -1,26 +1,30 @@
 import { onCleanup, onMount } from "solid-js"
-import { FileDiff } from "@pierre/diffs"
+import { FileDiff, getSingularPatch } from "@pierre/diffs"
 import type { PRFile } from "../types"
 
 type Props = {
   file: PRFile
 }
 
+function buildPatch(file: PRFile): string {
+  const a = `a/${file.Filename}`
+  const b = `b/${file.Filename}`
+  return `diff --git ${a} ${b}\n--- ${a}\n+++ ${b}\n${file.Patch}`
+}
+
 export function DiffView(props: Props) {
   let container!: HTMLDivElement
+  let instance: InstanceType<typeof FileDiff> | null = null
 
   onMount(() => {
-    const instance = new FileDiff() as unknown as HTMLElement & {
-      patch: string
-      filename: string
-    }
-    instance.patch = props.file.Patch
-    instance.filename = props.file.Filename
-    container.appendChild(instance)
+    const fileDiff = getSingularPatch(buildPatch(props.file))
+    instance = new FileDiff()
+    instance.hydrate({ fileDiff, fileContainer: container })
   })
 
   onCleanup(() => {
-    container.innerHTML = ""
+    instance?.cleanUp()
+    instance = null
   })
 
   return <div ref={container} />
